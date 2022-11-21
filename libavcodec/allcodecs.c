jjustman/ffmepg-vvdec-ac4

avcodec: add vvdec support in ffmpeg

libavcodec/allcodecs.c
@@ -183,6 +183,7 @@ extern const AVCodec ff_jv_decoder;
 extern const AVCodec ff_kgv1_decoder;
 extern const AVCodec ff_kmvc_decoder;
 extern const AVCodec ff_lagarith_decoder;
+extern const AVCodec ff_libvvdec_decoder;
 extern const AVCodec ff_ljpeg_encoder;
 extern const AVCodec ff_loco_decoder;
 extern const AVCodec ff_lscr_decoder;

3EMS2/FFmpeg

libavcodec/allcodecs.c
@@ -187,6 +187,7 @@ extern const FFCodec ff_jv_decoder;
 extern const FFCodec ff_kgv1_decoder;
 extern const FFCodec ff_kmvc_decoder;
 extern const FFCodec ff_lagarith_decoder;
+extern const FFCodec ff_libvvdec_decoder;
 extern const FFCodec ff_ljpeg_encoder;
 extern const FFCodec ff_loco_decoder;
 extern const FFCodec ff_lscr_decoder;