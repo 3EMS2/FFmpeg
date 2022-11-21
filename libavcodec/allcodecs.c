jjustman/ffmepg-vvdec-ac4

avcodec: add openVVC decoder
Signed-off-by: Mickael Raulet <mraulet@gmail.com>

libavcodec/allcodecs.c
@@ -184,6 +184,7 @@ extern const AVCodec ff_kgv1_decoder;
 extern const AVCodec ff_kmvc_decoder;
 extern const AVCodec ff_lagarith_decoder;
 extern const AVCodec ff_libvvdec_decoder;
+extern const AVCodec ff_libopenvvc_decoder;
 extern const AVCodec ff_ljpeg_encoder;
 extern const AVCodec ff_loco_decoder;
 extern const AVCodec ff_lscr_decoder;

3EMS2/FFmpeg

libavcodec/allcodecs.c
@@ -188,6 +188,7 @@ extern const FFCodec ff_kgv1_decoder;
 extern const FFCodec ff_kmvc_decoder;
 extern const FFCodec ff_lagarith_decoder;
 extern const FFCodec ff_libvvdec_decoder;
+extern const FFCodec ff_libopenvvc_decoder;
 extern const FFCodec ff_ljpeg_encoder;
 extern const FFCodec ff_loco_decoder;
 extern const FFCodec ff_lscr_decoder;