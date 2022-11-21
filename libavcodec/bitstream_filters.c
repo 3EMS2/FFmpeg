jjustman/ffmepg-vvdec-ac4

avformat: add support for vvc in isobmff and dash

libavcodec/bitstream_filters.c
@@ -36,6 +36,7 @@ extern const AVBitStreamFilter ff_extract_extradata_bsf;
 extern const AVBitStreamFilter ff_filter_units_bsf;
 extern const AVBitStreamFilter ff_h264_metadata_bsf;
 extern const AVBitStreamFilter ff_h264_mp4toannexb_bsf;
+extern const AVBitStreamFilter ff_vvc_mp4toannexb_bsf;
 extern const AVBitStreamFilter ff_h264_redundant_pps_bsf;
 extern const AVBitStreamFilter ff_hapqa_extract_bsf;
 extern const AVBitStreamFilter ff_hevc_metadata_bsf;

3EMS2/FFmpeg

libavcodec/bitstream_filters.c
@@ -65,6 +65,7 @@ extern const FFBitStreamFilter ff_vp9_raw_reorder_bsf;
 extern const FFBitStreamFilter ff_vp9_superframe_bsf;
 extern const FFBitStreamFilter ff_vp9_superframe_split_bsf;
 extern const FFBitStreamFilter ff_vvc_metadata_bsf;
+extern const FFBitStreamFilter ff_vvc_mp4toannexb_bsf;
 
 #include "libavcodec/bsf_list.c"
 